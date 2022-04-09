a846d917f11803466c2eee975f922143aa87abf4
Lars Brandt <zebrajaeger@zebrajaeger.de>
2022-04-09 09:03:03 +0200

    fix non zero joystick center

--- server-java/src/main/java/de/zebrajaeger/phserver/util/MathUtils.java
+++ server-java/src/main/java/de/zebrajaeger/phserver/util/MathUtils.java
@@ -7,4 +7,8 @@ public class MathUtils {
     public static float map(float rawValue, float rawMinValue, float rawMaxValue, float rangeMinValue, float rangeMaxValue) {
         return (rawValue - rawMinValue) * (rangeMaxValue - rangeMinValue) / (rawMaxValue - rawMinValue) + rangeMinValue;
     }
+
+    public static double map(double rawValue, double rawMinValue, double rawMaxValue, double rangeMinValue, double rangeMaxValue) {
+        return (rawValue - rawMinValue) * (rangeMaxValue - rangeMinValue) / (rawMaxValue - rawMinValue) + rangeMinValue;
+    }
 }

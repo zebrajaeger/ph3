93cb57cfb7de8f0116406139141a329a752b06dc
Lars Brandt <brandt@silpion.de>
2023-04-22 00:12:46 +0200

    [cleanup]

--- server-java/src/main/java/de/zebrajaeger/phserver/util/HexUtils.java
+++ server-java/src/main/java/de/zebrajaeger/phserver/util/HexUtils.java
@@ -1,51 +1,54 @@
 package de.zebrajaeger.phserver.util;
 
 /**
- * Thank you Baeldung! https://www.baeldung.com/java-byte-arrays-hex-strings
+ * Thank you Baeldung!
+ * <a href="https://www.baeldung.com/java-byte-arrays-hex-strings">
+ * https://www.baeldung.com/java-byte-arrays-hex-strings
+ * </a>
  */
 public class HexUtils {
 
-    private HexUtils() {
+  private HexUtils() {
+  }
+
+  public static String encodeHexString(byte[] byteArray) {
+    StringBuilder hexStringBuffer = new StringBuilder();
+    for (byte b : byteArray) {
+      hexStringBuffer.append(byteToHex(b));
+    }
+    return hexStringBuffer.toString();
+  }
+
+  public static byte[] decodeHexString(String hexString) {
+    if (hexString.length() % 2 == 1) {
+      throw new IllegalArgumentException("Invalid hexadecimal String supplied.");
     }
 
-    public static String encodeHexString(byte[] byteArray) {
-        StringBuilder hexStringBuffer = new StringBuilder();
-        for (byte b : byteArray) {
-            hexStringBuffer.append(byteToHex(b));
-        }
-        return hexStringBuffer.toString();
+    byte[] bytes = new byte[hexString.length() / 2];
+    for (int i = 0; i < hexString.length(); i += 2) {
+      bytes[i / 2] = hexToByte(hexString.substring(i, i + 2));
     }
+    return bytes;
+  }
 
-    public static byte[] decodeHexString(String hexString) {
-        if (hexString.length() % 2 == 1) {
-            throw new IllegalArgumentException("Invalid hexadecimal String supplied.");
-        }
+  public static String byteToHex(byte num) {
+    char[] hexDigits = new char[2];
+    hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
+    hexDigits[1] = Character.forDigit((num & 0xF), 16);
+    return new String(hexDigits);
+  }
 
-        byte[] bytes = new byte[hexString.length() / 2];
-        for (int i = 0; i < hexString.length(); i += 2) {
-            bytes[i / 2] = hexToByte(hexString.substring(i, i + 2));
-        }
-        return bytes;
+  public static byte hexToByte(String hexString) {
+    int firstDigit = toDigit(hexString.charAt(0));
+    int secondDigit = toDigit(hexString.charAt(1));
+    return (byte) ((firstDigit << 4) + secondDigit);
+  }
+
+  private static int toDigit(char hexChar) {
+    int digit = Character.digit(hexChar, 16);
+    if (digit == -1) {
+      throw new IllegalArgumentException("Invalid Hexadecimal Character: " + hexChar);
     }
-
-    public static String byteToHex(byte num) {
-        char[] hexDigits = new char[2];
-        hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
-        hexDigits[1] = Character.forDigit((num & 0xF), 16);
-        return new String(hexDigits);
-    }
-
-    public static byte hexToByte(String hexString) {
-        int firstDigit = toDigit(hexString.charAt(0));
-        int secondDigit = toDigit(hexString.charAt(1));
-        return (byte) ((firstDigit << 4) + secondDigit);
-    }
-
-    private static int toDigit(char hexChar) {
-        int digit = Character.digit(hexChar, 16);
-        if (digit == -1) {
-            throw new IllegalArgumentException("Invalid Hexadecimal Character: " + hexChar);
-        }
-        return digit;
-    }
+    return digit;
+  }
 }

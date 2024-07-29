ea5efd425464b493b16dca4c57c3dd890b2415ec
Lars Brandt <zebrajaeger@zebrajaeger.de>
2024-07-29 14:42:48 +0200

    [add] ssl support with self signed certificate

---
+++ server-java/src/main/java/de/zebrajaeger/phserver/config/ServerConfig.java
@@ -0,0 +1,23 @@
+package de.zebrajaeger.phserver.config;
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+
+    @Value("${http.port}")
+    private int httpPort;
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return server -> {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setPort(httpPort);
+            server.addAdditionalTomcatConnectors(connector);
+        };
+    }
+}

6e7371bdeaf5666c2d289bae89f8d867cfb39eb4
Lars Brandt <zebrajaeger@zebrajaeger.de>
2024-08-13 09:04:04 +0200

    [fix] fakecamera sends no events

--- server-java/src/main/java/de/zebrajaeger/phserver/data/CameraStatus.java
+++ server-java/src/main/java/de/zebrajaeger/phserver/data/CameraStatus.java
@@ -2,13 +2,11 @@ package de.zebrajaeger.phserver.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class CameraStatus {
     protected boolean focus;
     protected boolean trigger;
--- server-java/src/main/java/de/zebrajaeger/phserver/hardware/actor/FakeCamera.java
+++ server-java/src/main/java/de/zebrajaeger/phserver/hardware/actor/FakeCamera.java
@@ -1,58 +1,82 @@
 package de.zebrajaeger.phserver.hardware.actor;
 
 import de.zebrajaeger.phserver.data.CameraStatus;
+import de.zebrajaeger.phserver.event.CameraChangedEvent;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 @Service
 @ConditionalOnProperty("enable.camera.fake")
-//@Profile({"fake-cam"})
-public class FakeCamera extends PollingCamera implements Camera {
-    private final CameraStatus cameraStatus = new CameraStatus();
+@Slf4j
+public class FakeCamera implements Camera {
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public FakeCamera(ApplicationEventPublisher applicationEventPublisher) {
-        super(applicationEventPublisher);
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @PostConstruct
+    public void init() {
+        applicationEventPublisher.publishEvent(new CameraChangedEvent(new CameraStatus(false, false)));
     }
 
     @Override
     public void startFocus(int focusTimeMs) {
-        cameraStatus.setFocus(true);
-        executorService.schedule(() -> cameraStatus.setFocus(false), focusTimeMs,
-                TimeUnit.MILLISECONDS);
+        startFocus(focusTimeMs, null);
+    }
+
+    private void startFocus(int focusTimeMs, Consumer<Boolean> onFinish) {
+        if (focusTimeMs > 0) {
+            log.debug("Camera Focus on");
+            applicationEventPublisher.publishEvent(new CameraChangedEvent(new CameraStatus(true, false)));
+            executorService.schedule(() -> {
+                log.debug("Camera Focus off");
+                applicationEventPublisher.publishEvent(new CameraChangedEvent(new CameraStatus(false, false)));
+                if (onFinish != null) {
+                    onFinish.accept(true);
+                }
+            }, focusTimeMs, TimeUnit.MILLISECONDS);
+        } else {
+            if (onFinish != null) {
+                onFinish.accept(false);
+            }
+        }
     }
 
     @Override
     public void startTrigger(int triggerTimeMs) {
-        cameraStatus.setTrigger(true);
-        executorService.schedule(() -> cameraStatus.setTrigger(false), triggerTimeMs,
-                TimeUnit.MILLISECONDS);
+        startTrigger(triggerTimeMs, null);
+    }
+
+    private void startTrigger(int triggerTimeMs, Consumer<Boolean> onFinish) {
+        if (triggerTimeMs > 0) {
+            log.debug("Camera Trigger on");
+            applicationEventPublisher.publishEvent(new CameraChangedEvent(new CameraStatus(false, true)));
+            executorService.schedule(() -> {
+                log.debug("Camera Trigger off");
+                applicationEventPublisher.publishEvent(new CameraChangedEvent(new CameraStatus(false, false)));
+                if (onFinish != null) {
+                    onFinish.accept(true);
+                }
+            }, triggerTimeMs, TimeUnit.MILLISECONDS);
+        } else {
+            if (onFinish != null) {
+                onFinish.accept(false);
+            }
+        }
     }
 
     @Override
     public void startShot(int focusTimeMs, int triggerTimeMs) {
-        cameraStatus.setFocus(true);
-        executorService.schedule(() -> {
-            cameraStatus.setFocus(false);
-            cameraStatus.setTrigger(true);
-            executorService.schedule(() -> cameraStatus.setTrigger(false), triggerTimeMs,
-                    TimeUnit.MILLISECONDS);
-        }, focusTimeMs, TimeUnit.MILLISECONDS);
-    }
-
-    @Override
-    public void update() {
-        // ignore
-    }
-
-    @Override
-    public CameraStatus readCameraStatus() {
-        return cameraStatus;
+        startFocus(focusTimeMs, x1 -> startTrigger(triggerTimeMs, x2 -> log.debug("Shot done {},{}", x1, x2)));
     }
 }

db5d7028a8097452b12893e9bfe9e38561cd74a9
Lars Brandt <zebrajaeger@zebrajaeger.de>
2024-08-15 08:33:42 +0200

    [add] custom logo/icon

--- client-ng/src/app/app.module.ts
+++ client-ng/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, isDevMode} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -54,6 +54,7 @@ import {KeyboardComponent} from './ui/keyboard/keyboard.component';
 import { ShutdownComponent } from './screen/shutdown/shutdown.component';
 import { TouchpadComponent } from './ui/touchpad/touchpad.component';
 import { DeviceOrientationJoystickComponent } from './ui/device-orientation-joystick/device-orientation-joystick.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
     declarations: [
@@ -105,7 +106,13 @@ import { DeviceOrientationJoystickComponent } from './ui/device-orientation-joys
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FormsModule
+        FormsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [
         CameraService,
